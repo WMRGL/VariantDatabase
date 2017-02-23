edd08614ddcefd5190aadf6779aa4c360d4f0c17
Joseph Halstead <josephhalstead@gmail.com>
2017-02-23 12:41:37 +0000

    1) Added login and logout functionality 2)login required decorators on relevent views 3)updated html and css design e.g panels

--- VariantDatabase/urls.py
+++ VariantDatabase/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [ url(r'^$', views.home_page, name='home_page'),
 				url(r'^list_projects$', views.list_projects, name='list_projects'),
 				url(r'^project/(?P<pk>\d+)/$', views.list_batches, name='list_batches'),
 				url(r'^batch/(?P<pk>\d+)/$', views.list_batch_samples, name='list_batch_samples'),
+				url(r'^login/$', auth_views.login, {'template_name': 'VariantDatabase/login.html'}, name='login'),
+				url(r'^logout/$', auth_views.logout, {'template_name': 'VariantDatabase/logout.html'}, name='logout'),
+
+
 				
 
 ]
--- VariantDatabase/views.py
+++ VariantDatabase/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from VariantDatabase.models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def home_page(request):
 
 	return render(request, 'VariantDatabase/home_page.html', {})
 
-
+@login_required
 def list_projects(request):
 
 	all_projects = Project.objects.all()
 
 	return render(request, 'VariantDatabase/list_projects.html', {'all_projects': all_projects} )
 
-
+@login_required
 def list_batches(request, pk):
 
 	project = get_object_or_404(Project, pk=pk)
@@ -24,7 +26,7 @@ def list_batches(request, pk):
 	return render(request,'VariantDatabase/list_batches.html', {'batches_for_project': batches_for_project, 'project': project})
 
 
-
+@login_required
 def list_batch_samples(request, pk):
 
 	batch = get_object_or_404(Batch, pk=pk)
@@ -34,3 +36,5 @@ def list_batch_samples(request, pk):
 	samples_in_batch = Sample.objects.filter(batch = batch)
 
 	return render(request, 'VariantDatabase/list_batch_samples.html', {'batch': batch, 'samples_in_batch' : samples_in_batch})
+
+
