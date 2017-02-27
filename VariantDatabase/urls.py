97e4527e4924329a89d1fff842df1468fe502002
Joseph Halstead <josephhalstead@gmail.com>
2017-02-27 12:19:44 +0000

    1) Added view VCF file functionality. The suer can now load a VCF into a html table. Note this requires the cyvcf2 module to be installed. Code to extract VCF data placed in preliminary folder.

--- VariantDatabase/urls.py
+++ VariantDatabase/urls.py
@@ -5,7 +5,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [ url(r'^$', views.home_page, name='home_page'),
 				url(r'^list_projects$', views.list_projects, name='list_projects'),
 				url(r'^project/(?P<pk>\d+)/$', views.list_batches, name='list_batches'),
-				url(r'^batch/(?P<pk>\d+)/$', views.list_batch_samples, name='list_batch_samples'),
+				url(r'^batch/(?P<pk_project>\d+)/(?P<pk_batch>\d+)/$', views.list_batch_samples, name='list_batch_samples'),
+				url(r'^batch/(?P<pk_project>\d+)/(?P<pk_batch>\d+)/(?P<pk_sample>\d+)/$', views.list_sample_variants, name='list_sample_variants'),
+
+
+
 				url(r'^login/$', auth_views.login, {'template_name': 'VariantDatabase/login.html'}, name='login'),
 				url(r'^logout/$', auth_views.logout, {'template_name': 'VariantDatabase/logout.html'}, name='logout'),
 				url(r'^search/$', views.search_page, name='search_page'),
--- VariantDatabase/views.py
+++ VariantDatabase/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from VariantDatabase.models import *
 from django.contrib.auth.decorators import login_required
-
+import imp
+from cyvcf2.cyvcf2 import VCF
+extract_sample_data = imp.load_source('extract_sample_data', '/home/cuser/Documents/Project/Preliminary/cyvcf2/extract_sample_data.py')
+import dicttoxml
 
 
 @login_required
@@ -27,25 +30,33 @@ def list_batches(request, pk):
 
 
 @login_required
-def list_batch_samples(request, pk):
+def list_batch_samples(request, pk_project, pk_batch):
 
-	batch = get_object_or_404(Batch, pk=pk)
-
-	#project = get_object_or_404(Project, pk_project)
+	batch = get_object_or_404(Batch, pk=pk_batch)
 
 	samples_in_batch = Sample.objects.filter(batch = batch)
 
-	return render(request, 'VariantDatabase/list_batch_samples.html', {'batch': batch, 'samples_in_batch' : samples_in_batch})
+	return render(request, 'VariantDatabase/list_batch_samples.html', {'batch': batch, 'samples_in_batch' : samples_in_batch,})
 
 
-def list_sample_variants(request, pk):
+def list_sample_variants(request, pk_project, pk_batch, pk_sample):
+
+	batch = get_object_or_404(Batch, pk=pk_batch)
+
+	sample = get_object_or_404(Sample, pk=pk_sample)
+
+	vcf_file_path = batch.vcf_file
+
+	data = extract_sample_data.create_variant_list(vcf_file_path, sample.sample_name)
+
 
 	#get sample batch
 	#get vcf fiel path
 	#extract variants from vcf
 	#display in table
 
-	pass
+
+	return render(request, 'VariantDatabase/list_sample_variants.html', {'sample': sample, 'batch': batch, 'vcf_file_path': vcf_file_path, 'data': data})
 
 
 #finish this later
